4fdade1b4dbcc8d794ac03fdcbc7ef32323eed48
Ubuntu <ubuntu@ip-172-31-14-80.ap-south-1.compute.internal>
2023-04-07 17:16:58 +0000

    Add Eclipse src folder to repository

---
+++ src/main/java/dynamicwebproject/SEJALCAL.java
@@ -0,0 +1,10 @@
+package dynamicwebproject;
+
+public class SEJALCAL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SEJAL PROJECT");
+	}
+
+}
